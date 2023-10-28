25111dd371e4cf170eda41c7f62ec0aa54b8b184
Falcon <88769526+buckdan@users.noreply.github.com>
2023-10-28 13:41:21 +0000

    1

---
+++ Array/Array_1D/sonho.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int a[1000005];
+
+    for(int i = 1; i <= n; i++)
+    {
+        a[i] = i;
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        a[max(x,y)] = min(x,y);
+    }
+
+    int count1 = 0, count2 = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == i)
+            count1++;
+        else
+            count2++;
+    }
+
+    cout << count1 << endl;
+    cout << count2 << endl;
+
+    return 0;
+}
