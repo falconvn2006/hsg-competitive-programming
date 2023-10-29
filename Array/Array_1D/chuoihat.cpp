d6408a18cc3ce7e942d98778bce9fb4f882228c4
Falcon <88769526+buckdan@users.noreply.github.com>
2023-10-29 10:46:42 +0000

    complete data inp

--- Array/Array_1D/chuoihat.cpp
+++ Array/Array_1D/chuoihat.cpp
@@ -33,9 +33,25 @@ int main()
         if(arr[i] != 0)
         {
             vec.push_back(arr[i]);
-            cout << arr[i] << endl;
+            // cout << arr[i] << endl;
         }
     }
+
+    vector<int> a1, a2;
+
+    for(int i = 0; i < vec.size(); i += 2)
+    {
+        a1.push_back(vec[i]);
+        a2.push_back(vec[i+1]);
+    }
+
+    for(int i = 0; i < a1.size(); i++)
+        cout << a1[i] << " ";
+
+    cout << endl;
+
+    for(int i = 0; i < a1.size(); i++)
+        cout << a2[i] << " ";
 
     cout << endl;
 
