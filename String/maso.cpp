05968c2204480a40812aaefbd0523dacfbe67fc5
Falcon <88769526+buckdan@users.noreply.github.com>
2023-11-12 10:31:39 +0000

    commitment

---
+++ String/maso.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int k;
+    string s7;
+    string sk;
+
+    cin >> k;
+    cin >> s7;
+
+    int sum = 0;
+
+    for(int i = 0; i < 7; i++)
+    {
+        int x;
+        cin >> x;
+
+        sum += s7[i] * x;
+    }
+
+    cin >> sk;
+
+    int mod = sum % k;
+
+    char res;
+
+    for(int i = 0; i < sk.size(); i++)
+    {
+        if(mod/2 == i)
+        {
+            res = sk[i-1];
+            break;
+        }
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
